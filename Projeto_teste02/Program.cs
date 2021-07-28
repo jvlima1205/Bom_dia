b8cb6cec75fb174e67b44ed0ed05d98b88dc2f89
jvlima1205 <jvlima1205@gmail.com>
2021-07-28 14:23:13 -0300

    Tudo bom?

--- Projeto_teste02/Program.cs
+++ Projeto_teste02/Program.cs
@@ -7,6 +7,9 @@ namespace Projeto_teste02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Opa tudo bom?");
+            string bom = Console.ReadLine();
+
         }
     }
 }
